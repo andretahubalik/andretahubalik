73497e5ff0689b321ad35bbf3a4e72e818e6b5d3
andretahubalik <andretahubalik@gmail.com>
2024-08-05 00:40:25 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,283 @@
+<!DOCTYPE html>
+
+
+
+
+<a href="https://onenews.sch.id/sejarah-dan-keunikan-candi-gatotkaca-dieng/" style="display:none">Gatotkaca</a>
+<a href="https://narsis.id/ada-juga-jabat-tangan-di-konser-jkt48-tahun-2013/" style="display:none">JKT48</a>  
+<a href="https://pansos.id/tempat-wisata-indonesia-di-takengon-aceh-tengah-dengan-ciri-khas-kopi-gayo/" style="display:none">Kopi Gayo</a>
+<a href="https://forensik.id/satria-muda-britama-jakarta-raih-gelar-juara-liga-bola-basket-indonesia-ibl-2004/" style="display:none">Juara Liga Bola Basket</a>
+<a href="https://mainstream.id/game-hp-yang-cocok-untuk-anak-anak/" style="display:none">Game HP yang Cocok untuk Anak-Anak</a>
+<a href="https://balqisnews.sch.id/10-game-ramah-anak-tahun-2024/" style="display:none">10 Game Ramah Anak Tahun 2024</a>
+<a href="https://harpitnas.com/harpitnas-hari-kejepit-nasional/" style="display:none">Harpitnas</a>
+<a href="https://muntaber.com/muntaber-muntah-dan-berak/" style="display:none">Muntaber</a>
+<a href="https://hardiknas.com/peringatan-hardiknas-hari-pendidikan-nasional-untuk-kemajuan-dan-kesejahteraan-bangsa/" style="display:none">Hardiknas</a>  
+<a href="https://paspampres.com/paspampres-penjaga-kehormatan-keamanan-presiden/" style="display:none">Paspampres</a>
+<a href="https://pakcoy.com/sayur-pakcoy-manfaat-cara-menanam-dan-mengolah-resep-lezat/" style="display:none">Pakcoy</a>
+<a href="https://harkitnas.com" style="display:none">Harkitnas</a>
+<a href="https://tangkubanperahu.com" style="display:none">Tangkuban Perahu</a>
+<a href="https://sibolangit.com" style="display:none">Sibolangit</a>
+<a href="https://simarjarunjung.com" style="display:none">Simarjarunjung</a>
+<a href="https://siguragura.com" style="display:none">Sigura-gura</a>
+<a href="https://simanindo.com" style="display:none">Simanindo</a>
+<a href="https://padarincang.com" style="display:none">Padarincang</a>
+<a href="https://cilawu.com" style="display:none">Cilawu</a>
+<a href="https://cilengkrang.com" style="display:none">Cilengkrang</a>
+<a href="https://kolektor.id" style="display:none">Kolektor</a>
+<a href="https://pelukis.id" style="display:none">Pelukis</a>
+<a href="https://pancoran.id" style="display:none">Pancoran</a>
+<a href="https://kualitas.id" style="display:none">Kualitas</a>
+<a href="https://jasmani.id" style="display:none">Jasmani</a>
+<a href="https://cipanas.id" style="display:none">Cipanas</a>
+<a href="https://eksklusif.id" style="display:none">Eksklusif</a>
+<a href="https://inovatif.id" style="display:none">Inovatif</a>
+<a href="https://xenia.id" style="display:none">Xenia</a>
+<a href="https://wamena.id" style="display:none">Wamena</a>
+<a href="https://parapat.id" style="display:none">Parapat</a>
+<a href="https://penatapan.id" style="display:none">Penatapan</a>
+<a href="https://balige.id" style="display:none">Balige</a>
+<a href="https://ciomas.id" style="display:none">Ciomas</a>
+
+
+
+
+<html lang="en-US">
+<head>
+    <meta charset="UTF-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="USN Kolaka" content="Theme Starz">
+    <meta name="google-site-verification" content="USN Kolaka" />
+    <link href='http://fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
+    <link href="assets/css/font-awesome.css" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.css" type="text/css">
+    <link rel="stylesheet" href="assets/css/selectize.css" type="text/css">
+    <link rel="stylesheet" href="assets/css/owl.carousel.css" type="text/css">
+    <link rel="stylesheet" href="assets/css/vanillabox/vanillabox.css" type="text/css">
+    <link rel="stylesheet" href="assets/css/vanillabox/vanillabox.css" type="text/css">
+    <link rel="stylesheet" href="assets/css/style.css" type="text/css">
+    <title>USN Kolaka</title>
+    <link rel="icon" href="assets/usnlogo.png" type="image/x-icon">
+    <link rel="shortcut icon" href="assets/usnlogo.png" type="image/x-icon">
+    <style>
+        body {
+            font-size: 10pt;
+            color: #000; /* Warna teks default */
+        }
+        table {
+            width: 100%;
+            border-collapse: collapse;
+        }
+        table, th, td {
+            border: 1px solid black;
+        }
+        th, td {
+            padding: 8px;
+            text-align: left;
+        }
+        th {
+            background-color: #f2f2f2;
+            text-align: center; /* Perataan teks di tengah untuk elemen <th> */
+        }
+        td {
+            text-align: left; /* Perataan teks di kiri untuk elemen <td> */
+        }
+        /* Pastikan warna font dalam tabel sama dengan teks di luar tabel */
+        th, td {
+            color: #000;
+        }
+    </style>
+
+</head>
+
+<body class="page-homepage-carousel">
+<!-- Wrapper -->
+<div class="wrapper">
+<!-- Header -->
+<div class="navigation-wrapper">
+    
+
+<div class="secondary-navigation-wrapper">
+    <div class="container">
+        <div class="navigation-contact pull-left">Telepon :  <span class="opacity-70">0405-2321132</span></div>
+        <ul class="secondary-navigation list-unstyled">
+            <li><a href="https://usn-ic.usn.ac.id/" target="blank">Konferensi Internasional</a></li>
+            <li><a href="#">Kemahasiswaan</a></li>
+            <li><a href="https://perpus.usn.ac.id/" target="blank">Perpustakaan</a></li>
+            <li><a href="https://usn.ac.id/"><img src="assets/ina.png" alt="Indonesia" style="width:20px; height:auto;"></a> | <a href="https://usn.ac.id/en/"><img src="assets/ing.png" alt="Indonesia" style="width:20px; height:auto;"></a></li>
+        </ul>
+    </div>
+</div>
+
+    <div class="primary-navigation-wrapper">
+        <header class="navbar" id="top" role="banner">
+            <div class="container">
+                <div class="navbar-header">
+                    <button class="navbar-toggle" type="button" data-toggle="collapse" data-target=".bs-navbar-collapse">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                </div>
+                <a href="index.php"><img src="assets/usnbrand3.png" alt="Indonesia" style="width:250px; height:auto;"></a>
+                <nav class="collapse navbar-collapse bs-navbar-collapse navbar-right" role="navigation">
+                    <ul class="nav navbar-nav">
+                        <li class="active">
+                            <a href="index.php?view=beranda" class="has-child no-link">Beranda</a>
+                            <ul class="list-unstyled child-navigation">
+                                <li><a href="index.php?view=sambutan">Sambutan</a></li>
+                                <li><a href="index.php?view=berita-terkini">Berita Terkini</a></li>
+                            </ul>
+                        </li>
+                        <li>
+                            <a href="#" class=" has-child no-link">Tentang Kami</a>
+                            <ul class="list-unstyled child-navigation">
+                                <li><a href="index.php?view=visi-misi">Visi & Misi</a></li>
+                                <li><a href="index.php?view=sejarah">Sejarah</a></li>
+                                <li><a href="index.php?view=pimpinan">Pimpinan</a></li>
+                            </ul>
+                        </li>
+                        <li>
+                            <a href="#" class="has-child no-link">Fakultas</a>
+                            <ul class="list-unstyled child-navigation">
+                                <li><a href="index.php?view=fkip">Fakultas Keguruan dan Ilmu Pendidikan</a></li>
+                                <li><a href="index.php?view=fppp">Fakultas Pertanian, Perikanan dan Peternakan</a></li>
+                                <li><a href="index.php?view=fst">Fakultas Sains dan Teknologi</a></li>
+                                <li><a href="index.php?view=fisie">Fakultas Ilmu Sosial dan Ilmu Ekonomi</a></li>
+                                <li><a href="index.php?view=fhukum">Fakultas Hukum</a></li>
+                                <li><a href="index.php?view=fti">Fakultas Teknologi Informasi</a></li>
+                            </ul>
+                        </li>
+                        <li>
+                            <a href="#" class="has-child no-link">Lembaga</a>
+                            <ul class="list-unstyled child-navigation">
+                                <li><a href="index.php?view=lppm">Penelitian & Pengabdian Masyarakat</a></li>
+                                <li><a href="index.php?view=lpmp">Penjaminan Mutu & dan Pengembangan Pembelajaran</a></li>
+                            </ul>
+                        </li>
+                        <li>
+                            <a href="#" class="has-child no-link">Berita</a>
+                            <ul class="list-unstyled child-navigation">
+                                <li><a href="index.php?view=berita">Berita Universitas</a></li>
+                                <li><a href="index.php?view=acara">Dokumentasi</a></li>
+                            </ul>
+                        </li>
+                        <li>
+                            <a href="index.php?view=hubkami">Hubungi Kami</a>
+                        </li>
+                    </ul>
+                </nav><!-- /.navbar collapse-->
+            </div><!-- /.container -->
+        </header><!-- /.navbar -->
+    </div><!-- /.primary-navigation -->
+    <div class="background">
+        <img src="assets/img/background-city.png"  alt="background">
+    </div>
+</div>
+<!-- end Header -->
+
+<?php
+    if (isset($_GET['view'])) {
+        $view = $_GET['view'];
+    }else{
+        $view = '';
+    }
+
+    if($view == 'beranda' || $view == ''){
+        include "form/usn-beranda.php";
+    }elseif ($view == 'sambutan'){
+        include "form/usn-sambutan.php";
+    }elseif ($view == 'berita-terkini'){
+        include "form/usn-berita-terkinii.php";
+    }elseif ($view == 'visi-misi'){
+        include "form/usn-visi-misi.php";
+    }elseif ($view == 'sejarah'){
+        include "form/usn-sejarah.php";
+    }elseif ($view == 'fkip'){
+        include "form/usn-fkip.php";
+    }elseif ($view == 'fppp'){
+        include "form/usn-fppp.php";
+    }elseif ($view == 'fst'){
+        include "form/usn-fst.php";
+    }elseif ($view == 'fisie'){
+        include "form/usn-fisie.php";
+    }elseif ($view == 'fhukum'){
+        include "form/usn-fhukum.php";
+    }elseif ($view == 'fti'){
+        include "form/usn-fti.php";
+    }elseif ($view == 'lppm'){
+        include "form/usn-lppm.php";
+    }elseif ($view == 'lpmp'){
+        include "form/usn-lpmp.php";
+    }elseif ($view == 'hubkami'){
+        include "form/usn-hubkami.php";
+    }elseif ($view == 'berita'){
+        include "form/usn-berita.php";
+    }elseif ($view == 'usn-berita'){
+        include "form/usn-berita-detail.php";
+    }elseif ($view == 'pimpinan'){
+        include "form/usn-pimpinan.php";
+    }elseif ($view == 'acara'){
+        include "form/usn-acara.php";
+    }else{
+        echo "halaman belum ada";
+    }
+?>
+
+<!-- Footer -->
+<footer id="page-footer">
+    <section id="footer-content">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6 col-sm-4">
+                    <aside>
+                        <header><h4>Hubungi Kami</h4></header>
+                        <address>
+                            <strong>Universitas Sembilanbelas November Kolaka</strong>
+                            <br><br>
+                            <span>Jl. Pemuda No. 339 Kolaka, Indonesia</span>
+                            <br>
+                            <abbr title="Telephone">Telephone:</abbr> +62 (405) 2321132
+                            <br>
+                            <abbr title="Email">Email:</abbr> <a href="#">rektorat@usn.ac.id</a>
+                        </address>
+                    </aside>
+                </div>
+                <div class="col-md-6 col-sm-4">
+                    <aside>
+                        <header><h4></h4></header>
+                        <address>
+                            <span><a href="https://usnkolaka.siakadcloud.com/gate/login" target="blank">SISAKTI</a></span><br>
+                            <span><a href="https://lppm.usn.ac.id/simpel/" target="blank">SIMPEL</a></span><br>
+                            <span><a href="#">PPID</a></span><br>
+                            <span><a href="https://sidu.usn.ac.id/" target="blank">SIDU</a></span><br>
+                            <span><a href="https://siakad.usn.ac.id/" target="blank">SIAKAD (lama)</a></span>
+                        </address>
+                    </aside>
+                </div>
+            </div><!-- /.row -->
+        </div><!-- /.container -->
+        <div class="background"><img src="assets/img/background-city.png" class="" alt=""></div>
+    </section>
+</footer>
+<!-- end Footer -->
+
+</div>
+<!-- end Wrapper -->
+
+<script type="text/javascript" src="assets/js/jquery-2.1.0.min.js"></script>
+<script type="text/javascript" src="assets/js/jquery-migrate-1.2.1.min.js"></script>
+<script type="text/javascript" src="assets/bootstrap/js/bootstrap.min.js"></script>
+<script type="text/javascript" src="assets/js/selectize.min.js"></script>
+<script type="text/javascript" src="assets/js/owl.carousel.min.js"></script>
+<script type="text/javascript" src="assets/js/jquery.validate.min.js"></script>
+<script type="text/javascript" src="assets/js/jquery.placeholder.js"></script>
+<script type="text/javascript" src="assets/js/jQuery.equalHeights.js"></script>
+<script type="text/javascript" src="assets/js/icheck.min.js"></script>
+<script type="text/javascript" src="assets/js/jquery.vanillabox-0.1.5.min.js"></script>
+<script type="text/javascript" src="assets/js/retina-1.1.0.min.js"></script>
+
+<script type="text/javascript" src="assets/js/custom.js"></script>
+
+</body>
+</html>
